c07e08d0ae8231e19a7c0298b8d100345ee55352
Michael Thane <mthane@int.lin-magdeburg.de>
2021-04-28 09:00:25 +0200

    first scraping approach

--- Rscraping/app.R
+++ Rscraping/app.R
@@ -8,42 +8,84 @@
 #
 
 library(shiny)
+library(tidyverse)  
+library(rvest)    
+library(stringr)   
+# Verbose regular expressions
+library(rebus)     
+# Eases DateTime manipulation
+library(lubridate)
 
+library(DT)
+library(rjson)
+library(purrr)
+
+library(htm2txt)
+library(XML)
+library(data.table)
+source("RedditScraper.R")
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+textInput("reddit","Reddit","de"),
+numericInput("nposts","Number of posts",10),
+actionButton("scrape_json","Scrape"),
+DTOutput("jsonprint")
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    #download json file
+    scrape_json <- observeEvent(input$scrape_json,{
+        download.file(paste("https://www.reddit.com/r/",
+                            input$reddit,
+                            "/new/.json?limit=",
+                            input$nposts,sep=""),
+                      "test.json")
     })
+    
+    data <- reactive({
+        jsonfile = fromJSON(file = "test.json")
+        ndocuments <- length(jsonfile$data$children)
+        dfs <- list()
+        df <- data.frame()
+        for (i in 1:ndocuments){
+            data <- jsonfile$data$children[i][[1]]$data
+            print(names(data))
+            columns <- names(data)[1:10]
+            
+            list_data <- data[columns]%>%as.list()
+            
+            dfs[[i]]<-list_data%>%replace_na("")%>%as.data.frame()
+            
+            
+        }
+        rbindlist(dfs,fill=TRUE)
+        
+    })
+    
+    output$jsonprint <- renderDataTable({
+        data()
+        
+        })
+
+        
+    
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
+
+
