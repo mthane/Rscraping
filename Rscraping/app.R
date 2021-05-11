8ee9cd04232cf06c2d61884e4bb5ff411e4f92fc
Michael Thane <mthane@int.lin-magdeburg.de>
2021-05-11 18:22:18 +0200

    Scraping with pushshift API

--- Rscraping/app.R
+++ Rscraping/app.R
@@ -6,7 +6,6 @@
 #
 #    http://shiny.rstudio.com/
 #
-
 library(shiny)
 library(tidyverse)  
 library(rvest)    
@@ -15,14 +14,15 @@ library(stringr)
 library(rebus)     
 # Eases DateTime manipulation
 library(lubridate)
-
 library(DT)
 library(rjson)
 library(purrr)
-
 library(htm2txt)
 library(XML)
 library(data.table)
+library("jsonlite")
+library(shinyTime)
+
 source("RedditScraper.R")
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -30,48 +30,109 @@ ui <- fluidPage(
 
 textInput("reddit","Reddit","de"),
 numericInput("nposts","Number of posts",10),
+dateInput("time_from", "From", value = Sys.time()),
+dateInput("time_to", "To", value= Sys.time()),
 actionButton("scrape_json","Scrape"),
-DTOutput("jsonprint")
+DTOutput("jsonprint2"),
+DTOutput("tableAPI"),
+textOutput("time_test")
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
+
+    # FIRST SOLUTION
     #download json file
-    scrape_json <- observeEvent(input$scrape_json,{
+    scrape_json1 <- observeEvent(input$scrape_json,{
         download.file(paste("https://www.reddit.com/r/",
                             input$reddit,
                             "/new/.json?limit=",
                             input$nposts,sep=""),
                       "test.json")
+        
+        
     })
     
-    data <- reactive({
+    
+    data1 <- reactive({
         jsonfile = fromJSON(file = "test.json")
         ndocuments <- length(jsonfile$data$children)
         dfs <- list()
         df <- data.frame()
         for (i in 1:ndocuments){
             data <- jsonfile$data$children[i][[1]]$data
-            print(names(data))
             columns <- names(data)[1:10]
-            
             list_data <- data[columns]%>%as.list()
-            
             dfs[[i]]<-list_data%>%replace_na("")%>%as.data.frame()
-            
-            
         }
         rbindlist(dfs,fill=TRUE)
+    })
+    
+    
+    # SECOND SOLUTION
+    
+    scrape_json2 <- eventReactive(input$scrape_json,
+                                  {
+                                     fromJSON(url(paste("https://www.reddit.com/r/",
+                                                        input$reddit,
+                                                        "/new/.json?limit=",
+                                                        input$nposts,sep="")),flatten = T)
+                                      
+                                  })
+    
+
+    data2 <- reactive({
+        scrape_json2()$data$children%>%
+        select("data.subreddit",
+               "data.title",
+               "data.author",
+               "data.selftext",
+               "data.name",
+               "data.id",
+               "data.domain",
+               "data.url",
+               "data.created",
+               "data.created_utc",
+               "data.upvote_ratio",
+               "data.ups",
+               "data.score",
+               "data.num_comments"
+               )
         
     })
     
-    output$jsonprint <- renderDataTable({
-        data()
-        
+    
+    
+    output$jsonprint2 <- renderDataTable({
+        data2()
         })
-
+    
+    scrape_pushshiftAPI <- eventReactive(input$scrape_json,
+                                         {
+                                             pushshift_url = paste("https://api.pushshift.io/reddit/search/submission/?",
+                                                                   "before=",
+                                                                   paste0(as.numeric(input$time_to),"00000"),
+                                                                   "&after=",
+                                                                   paste0(as.numeric(input$time_from),"00000"),
+                                                                   "&size=",
+                                                                   input$nposts,
+                                                                   "&subreddit=",
+                                                                   input$reddit,sep="")
+                                             print(pushshift_url)
+                                             fromJSON(url(pushshift_url),flatten = T)
+                                             
+                                         })
+    
+    
+    output$tableAPI <- renderDataTable({
+        scrape_pushshiftAPI()$data
         
+    })
+    output$time_test <- renderPrint({
+        
+        print(as.numeric(input$time_from))
+    })
     
 }
 
